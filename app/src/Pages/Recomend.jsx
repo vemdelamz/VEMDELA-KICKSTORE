17b7b502b90e0e68f6dab597ff3ac5cb4bb009fb
vemdelamz <151420825+vemdelamz@users.noreply.github.com>
2024-01-02 21:46:19 +0000

    new commit first versiion

--- app/src/Pages/Recomend.jsx
+++ app/src/Pages/Recomend.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Tshirt1 from '../assets/images/ts.jpg'
-import Tshirt2 from '../assets/images/tsh.jpg'
+import Sport from '../assets/images/sport.jpg'
+import Kick from '../assets/images/kick.jpg'
 
 
 function RecommendedContent() {
@@ -11,19 +12,19 @@ function RecommendedContent() {
             <div className='lado-A'>
                 <div>
                     <a href='/collections/sneakers'>
-                    <img className='img' src={Tshirt1}/>
+                    <img className='img'src={Kick} />
                     </a>
                 </div>
                 <div>
                 <a href='/collections/sport'>
-                        <img className='img'  src={Tshirt2}/>
+                        <img className='img'  src={Sport}/>
                     </a>
                    
                 </div>
             </div>
             <div className='lado-B'> 
             <a href='/collections/tshirts'>
-                    <img className='img2'  src={Tshirt1}/>
+                    <img className='img2' src={Tshirt1} />
                 </a>
             </div>
         </div>
