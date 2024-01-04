3bcbf2b3f5d224a6d4609f22b3af526c0c4efb08
vemdelamz <151420825+vemdelamz@users.noreply.github.com>
2024-01-04 20:12:23 +0000

    new commit first versiion

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,24 +1,28 @@
-// hydrogen.config.ts
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
-import {defineConfig} from '@shopify/hydrogen/config';
-
-/* The `defineConfig` function is an optional utility that provides types for the configuration object. */
 export default defineConfig({
-  /* The routes defined by Vite's import.meta.globEager method. */
-  routes: '/src/routes',
-  /* The information that your app needs to connect to the Storefront API. */
   shopify: {
-    /* The domain of your Shopify store */
-    storeDomain: '{shop_domain}.myshopify.com',
-    /* Your app's public Storefront API access token. Authenticates browser and client requests. */
-    storefrontToken: Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN,
-    /* Your app's private Storefront API server (delegate access) token. Authenticates server requests. */
-    privateStorefrontToken: Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
-    /* The Storefront API version that your app uses */
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain:
+      // @ts-ignore
+      process?.env?.PUBLIC_STORE_DOMAIN || 'personalise-engrave-services.myshopify.com',
+    storefrontToken:
+      // @ts-ignore
+      process?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
+      '30cfd96af1e62b025026439eb6fab200',
+    privateStorefrontToken:
+      // @ts-ignore
+      process?.env?.PRIVATE_STOREFRONT_API_TOKEN,
     storefrontApiVersion: '2022-07',
-    /* (Optional) The unique ID for the storefront. Only gets assigned by Oxygen */
-    storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
+    // @ts-ignore
+    storefrontId: process?.env?.PUBLIC_STOREFRONT_ID,
   },
-  /* A path to a custom page to render when the server encounters an unhandled exception */
-  serverErrorPage: '/src/Error.jsx',
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
 });
