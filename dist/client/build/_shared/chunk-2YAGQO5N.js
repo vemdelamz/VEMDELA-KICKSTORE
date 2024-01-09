6cce99f02dbf4b49955a28ea194526e5da696d1c
vemdelamz <151420825+vemdelamz@users.noreply.github.com>
2024-01-09 17:24:23 +0000

    novo

--- dist/client/build/_shared/chunk-2YAGQO5N.js
+++ dist/client/build/_shared/chunk-2YAGQO5N.js
@@ -1,1 +1,1 @@
-//# sourceMappingURL=/build/_shared/chunk-2YAGQO5N.js.map
+
--- dist/client/build/_shared/chunk-VQSCELNZ.js
+++
@@ -1,2 +0,0 @@
-var i="/build/_assets/line-XK73GVJ4.png";var Z="/build/_assets/cart-JQJPR3SV.png";export{i as a,Z as b};
-
